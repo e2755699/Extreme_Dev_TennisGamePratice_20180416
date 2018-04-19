fdb5ad54caa6791987df4ce1a9e32b330b442aa6
dustin.liu62 <dustin.liu62@TI03BY45>
2018-04-19 22:17:17 +0800

    test casee "Thirty All"

--- Extreme_Dev_TennisGamePratice_20180416/UnitTest1.cs
+++ Extreme_Dev_TennisGamePratice_20180416/UnitTest1.cs
@@ -63,6 +63,14 @@ namespace Extreme_Dev_TennisGamePratice_20180416
             ScoreShouldBe("Fifteen All");
         }
 
+        [TestMethod]
+        public void Thirty_All()
+        {
+            GivenFirstPlayerScore(2);
+            GivenSecondPlayerScore(2);
+            ScoreShouldBe("Thirty All");
+        }
+
         private void GivenSecondPlayerScore(int times)
         {
             for (int i = 0; i < times; i++)
